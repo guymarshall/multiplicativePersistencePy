98c07117d2a38b0453568185c57c3ac40e4369ca
Guy Marshall <guymarshall9@gmail.com>
2022-07-19 18:52:00 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+def product(input):
+    result = 1
+
+    # get each digit by mid instead of string conversion
+    while input > 0:
+        result *= input % 10
+        input /= 10
+    return result
+
+def multiplicative_persistence(user_input):
+    steps = 0
+
+    # 10 is smallest double digit number
+    while user_input >= 10:
+        user_input = product(user_input)
+        steps += 1
+    return steps
+
+def main():
+    ## largest step count discovered = 277777788888899
+
+    highest_steps_count = 0
+    highest_steps_number = 0
+
+    # start = 312300000000
+    # start = 650000000000
+    # start = 2873700000000
+    # start = 5740000000000
+    # start = 5745600000000
+    # start = 6108200000000
+    start = 6111000000000
+    # finish = 1000000000000000
+    finish = 277777788888899
+
+    for number in range(start, finish + 1):
+        # print(f"{number}: {multiplicative_persistence(number)}")
+        if multiplicative_persistence(number) > highest_steps_count:
+            highest_steps_count = multiplicative_persistence(number)
+            highest_steps_number = number
+        if number % 1000000 == 0:
+            print(f"Upto {number} so far: {highest_steps_number}")
+    print(f"Highest step count: {highest_steps_number} at {highest_steps_count}")
+
+
+if __name__ == "__main__":
+    main()
