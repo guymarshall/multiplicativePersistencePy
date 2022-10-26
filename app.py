def04ea878b5c053f7fd1bc83b1128e926df8b7f
Guy Marshall <guymarshall9@gmail.com>
2022-10-26 10:55:24 +0100

    Change to using while loop

--- app.py
+++ app.py
@@ -27,27 +27,20 @@ def multiplicative_persistence(user_input):
 def main():
     ## largest step count discovered = 277777788888899
 
-    highest_steps_count = 0
-    highest_steps_number = 0
+    highest_steps_count = 11
+    highest_steps_number = 277777788888899
 
-    # start = 312300000000
-    # start = 650000000000
-    # start = 2873700000000
-    # start = 5740000000000
-    # start = 5745600000000
-    # start = 6108200000000
-    start = 6111000000000
-    # finish = 1000000000000000
-    finish = 277777788888899
+    number = 0
 
-    for number in range(start, finish + 1):
+    while highest_steps_count < 12:
         # print(f"{number}: {multiplicative_persistence(number)}")
         result = multiplicative_persistence(number)
         if result > highest_steps_count:
             highest_steps_count = result
             highest_steps_number = number
         if number % 1000000 == 0:
-            print(f"Upto {number} so far: {highest_steps_number}")
+            print(f"Upto {number} so far with {result} steps")
+        number += 1
     print(f"Highest step count: {highest_steps_number} at {highest_steps_count}")
 
 
